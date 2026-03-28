ab9040b78424c2d4f8d0bafdf185999917c8a3e0
ignatgift <gorborukovignst@gmail.com>
2026-03-29 00:59:03 +0600

    fix: Add explicit ComponentScan for controllers

--- src/main/java/ru/goidaai/test_backend/TestBackendApplication.java
+++ src/main/java/ru/goidaai/test_backend/TestBackendApplication.java
@@ -2,8 +2,10 @@ package ru.goidaai.test_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ru.goidaai.test_backend")
 public class TestBackendApplication {
 
 	public static void main(String[] args) {
