4770e66dabb762cdc1247989c9d6fc3518cf2f80
ignatgift <gorborukovignst@gmail.com>
2026-04-01 11:59:11 +0600

    fix: добавить импорт AvatarUploadResponse в SettingsController

--- src/main/java/ru/goidaai/test_backend/adapter/in/rest/SettingsController.java
+++ src/main/java/ru/goidaai/test_backend/adapter/in/rest/SettingsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import ru.goidaai.test_backend.dto.AvatarUploadResponse;
 import ru.goidaai.test_backend.dto.UpdateSettingsRequest;
 import ru.goidaai.test_backend.dto.UserDTO;
 import ru.goidaai.test_backend.dto.UserSettingsDTO;
